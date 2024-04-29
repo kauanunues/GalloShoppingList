6320bc23983f27af56e6eaaf08b777ca171c0ec6
kauannunes <marykakinho@gmail.com>
2024-04-29 11:58:24 -0300

    Finalizada a tela de splash

--- src/app/index.js
+++ src/app/index.js
@@ -4,11 +4,22 @@ import {Link} from "expo-router";
 export default function Welcome() {
   return (
     <View style={styles.container}>
-      <Text>Bem vindo</Text>
-      <Link href={"/home"}>
-      <Text> Acessar </Text>
+      <View style={styles.imagecontainer}>
+      <Image
+        source={requestAnimationFrame("../assets/avatar.png")}
+        style={styles.Image}
+      />
+      </View>
+      <View style={styles.content}>
+        <Text  style={styles.title}>Gallo Shopping List</Text>
+        <Text style={styles.text}>
+          Monte Sua lista de compras e não esqueça mais o que prescisa comprar ao sair de casa!
+        </Text>
+      </View>
+      <Link style={styles.button}href={"/home"}>
+      <Text> style={styles.buttonText} Acessar </Text>
       </Link>
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor= '#000' />
     </View>
   );
 }
@@ -16,8 +27,48 @@ export default function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
+  },
+  imagecontainer:{
+    flex:2 ,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Image :{
+    width:230,
+    height:230,
+    borderRadius:115,
+    resizeMode:'cover',
+  
+  },
+  content:{
+    felx:1,
+    backgroundColor:'#dadada',
+    borderTopLeftRadius:25,
+    borderTopRightRadius:25,
+    paddingHorizontal:'5%',
+  },
+  title:{
+    fontSize: 30,
+    fontWeigth
+  },
+  text:{
+    color: 'gray',
+    fontSize: 16,
+  },
+  button:{
+    position: 'absolute',
+    backgroundColor: '#000',
+    borderRadius: 50,
+    bottom: '15%',
+    paddingVertical: 15,
+    width: '60%',
+    alingSelf: 'center',
+    textAlign: 'center',
+  },
+  buttonText:{
+    color: '#fff',
+    fontSize: '',
+
   },
 });
