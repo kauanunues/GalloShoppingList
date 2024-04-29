1be0c72037fc9ab2de846a0a9a25f0dfd71ddf24
kauannunes <marykakinho@gmail.com>
2024-04-29 12:25:36 -0300

    ....

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -1,11 +1,34 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+{
+StyleSheet
+Text,
+View,
+SafeAreaView,
+ImageBackground,
+TextInput,
+TouchableOpacity
+
+}  'react-native';
+import { StatusBar } from 'expo-status-bar';
+import{ Ionicons} from '@expo/vector-icons';
+
 
 export default function home() {
   return (
-    <View>
-      <Text>Pagina Inicial</Text>
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      <imageBackground
+      source={require('../../assets/background.jpg')}
+      resizeMOde='repeat'
+      style={{flex:1,justifyContent: 'flex-start'}}
+      >
+       <View style={styles.footer}>
+        <Text style={styles.title}>Lista de Produtos</Text>
+        <Ionicons name="trash" size={32} color='#ffff' />
+       </View>
+      </imageBackground>
+      <StatusBar style="light" backgroundColor= '#000' />
+    </SafeAreaView>
   )
 }
 
